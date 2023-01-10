6a5a557798ec0d9e5cb29af1642bb1baaa16d0c2
Alvin Galit <aroblesgalit@gmail.com>
2023-01-09 21:19:25 -0800

    Stored tasks into localStorage

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@ type Task = {
 const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.querySelector('#new-task-form') as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
-const tasks: Task[] = [];
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
 
 form?.addEventListener('submit', e => {
   e.preventDefault();
@@ -34,12 +35,22 @@ function addListItem(task: Task) {
   const label = document.createElement('label');
   const checkbox = document.createElement('input');
   checkbox.addEventListener('change', () => {
-    task.completed = checkbox.checked
-    console.log(tasks);
+    task.completed = checkbox.checked;
+    saveTasks();
   });
   checkbox.type = 'checkbox';
   checkbox.checked = task.completed;
   label.append(checkbox, task.title);
   item.append(label);
   list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem('TASKS');
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
 }
