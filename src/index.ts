f59009bd4413983f8db5351ca517e39bb2e08a0a
Alvin Galit <aroblesgalit@gmail.com>
2023-01-09 20:34:22 -0800

    Created variables for each element

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+// import { v4 as uuidV4 } from 'uuid'
 
+// console.log(uuidV4());
+
+const list = document.querySelector('#list');
+const form = document.querySelector('#new-task-form');
+const title = document.querySelector('#new-task-title');
