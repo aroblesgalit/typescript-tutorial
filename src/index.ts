51b33bc79ca7326b04f92611fcfb5b6cdae393e2
Alvin Galit <aroblesgalit@gmail.com>
2023-01-09 20:44:18 -0800

    Created task object with id, title, completed, and createdAt props

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import { v4 as uuidV4 } from 'uuid'
+import { v4 as uuidV4 } from 'uuid'
 
 // console.log(uuidV4());
 
@@ -11,5 +11,10 @@ form?.addEventListener('submit', e => {
 
   if (input?.value == '' || input?.value == null) return;
 
-  
+  const task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
 });
