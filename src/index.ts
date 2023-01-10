3db7ef8c0aead9688f1fb1cc88ecce9b60eeaa6e
Alvin Galit <aroblesgalit@gmail.com>
2023-01-09 20:42:49 -0800

    Added eventlistener to form and check for valid input

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,11 @@
 const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.querySelector('#new-task-form') as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
+
+form?.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (input?.value == '' || input?.value == null) return;
+
+  
+});
