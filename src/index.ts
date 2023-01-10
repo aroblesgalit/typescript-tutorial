d27efc012df1f237e5200e64d1ab515bf1deecdb
Alvin Galit <aroblesgalit@gmail.com>
2023-01-09 20:39:03 -0800

    Added type checks for each element

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@
 
 // console.log(uuidV4());
 
-const list = document.querySelector('#list');
-const form = document.querySelector('#new-task-form');
-const title = document.querySelector('#new-task-title');
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.querySelector('#new-task-form') as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
